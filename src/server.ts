65da89b964babe10765b5bb1ed1924f9ae6881ee
Urias Pereira <uriasluis64@gmail.com>
2024-05-27 16:44:54 -0300

    cria estrutura da aplicação

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log(`Server listening at http://0.0.0.0:3333`)
+})
