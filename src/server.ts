4ba103a1f5598573c612d7385c6759d4843ba512
Urias Pereira <uriasluis64@gmail.com>
2024-05-28 09:30:30 -0300

    tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,6 +12,6 @@ app.get('/hello', async () => {
   return transaction
 })
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`Server listening at http://0.0.0.0:3333`)
 })
