76435182a0bed9256d0169c30a3b48fc256b89ca
Urias Pereira <uriasluis64@gmail.com>
2024-05-28 12:18:18 -0300

    criação de transações

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from './routes/transaction'
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`Server listening at http://0.0.0.0:3333`)
